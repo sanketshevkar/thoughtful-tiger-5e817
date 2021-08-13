3d7b063829e67711759af20215a9e287d147dd1e
Stackbit Code Editor <projects@stackbit.com>
2021-08-13 06:47:42 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -24,9 +24,6 @@ export default class Page extends React.Component {
                     </div>
                 </article>
             </Layout>
-            <div data-snack-id="@sanketshevkar/unnamed-snack" data-snack-platform="web" data-snack-preview="true" data-snack-theme="light" style="overflow:hidden;background:#F9F9F9;border:1px solid var(--color-border);border-radius:4px;height:505px;width:100%"></div>
-<Script async src="https://snack.expo.dev/embed.js"></Script>
-            </Head>
         );
     }
 }
