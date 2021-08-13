375d57e53a049e37ec9d89a0714e982298b9ae9c
Stackbit Code Editor <projects@stackbit.com>
2021-08-13 06:01:57 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -20,7 +20,7 @@ export default class Page extends React.Component {
                         {markdownContent && <div className="page__body text-block">{markdownify(markdownContent)}</div>}
                     </div>
                     <div data-snack-id="@sanketshevkar/unnamed-snack" data-snack-platform="web" data-snack-preview="true" data-snack-theme="light" style="overflow:hidden;background:#F9F9F9;border:1px solid var(--color-border);border-radius:4px;height:505px;width:100%"></div>
-<script async src="https://snack.expo.dev/embed.js"></script>
+                    <script async src="https://snack.expo.dev/embed.js"></script>
                 </article>
             </Layout>
         );
